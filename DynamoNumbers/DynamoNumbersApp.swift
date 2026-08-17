2d606d2a113580a768b5d572ba6efdaf7831abdd
mdavy <maria.davydenko@gmail.com>
2026-08-16 23:08:51 -0400

    Initial Commit

---
+++ DynamoNumbers/DynamoNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamoNumbersApp.swift
+//  DynamoNumbers
+//
+//  Created by maria davydenko on 8/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamoNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynamoNumbersTests/DynamoNumbersTests.swift
@@ -0,0 +1,19 @@
+//
+//  DynamoNumbersTests.swift
+//  DynamoNumbersTests
+//
+//  Created by maria davydenko on 8/16/26.
+//
+
+import Testing
+@testable import DynamoNumbers
+
+struct DynamoNumbersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
